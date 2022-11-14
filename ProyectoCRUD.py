14720b6af9c19f2d5433d6d0cb6db9206e6eff0d
Antonio Martinez <antonio_halo1@hotmail.com>
2022-11-14 17:56:19 -0600

    Se le agregaron las primeras caracteristicas a la interface tales como: un titulo, que no se pueda alterar las dimensiones de la interface y un icono.

--- ProyectoCRUD.py
+++ ProyectoCRUD.py
@@ -1,1 +1,19 @@
 #Versión 0.0
+from tkinter import * #Importamos de la libreriaa tkinter todos los modulos
+
+#--------------- Configuraciones iniciales --------------- 
+raiz = Tk() #TK() viene siendo como la ventana en si
+raiz.title("CRUD en python")
+raiz.resizable(False,False)#Para que no se pueda alterar el ancho y largo de la interface
+raiz.iconbitmap("C:/Users/anton/OneDrive/Escritorio/Curso de Python/Video 59 - Practica guiada I/icono.ico")
+
+#--------------- Frame --------------- 
+miFrame = Frame(raiz, width=400, height=550)#Frame() viene siendo una hoja interna sobre la cual ahora si ponemos los elementos que conforman nuestra interface
+miFrame.config(bg="#34495E")
+miFrame.pack() #.pack() sirve para... 
+
+#--------------- Barra de menu --------------- 
+
+
+
+raiz.mainloop()#Este metodo se queda esperando a que el usuario le pase un dato
