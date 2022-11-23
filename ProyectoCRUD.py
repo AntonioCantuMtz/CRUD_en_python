fe7d969862fbdad0a870f7db9e63081fa9e61dcf
Antonio Martinez <antonio_halo1@hotmail.com>
2022-11-23 16:30:26 -0600

    Se agregaron los label ya con su fuente y tipo de letra

--- ProyectoCRUD.py
+++ ProyectoCRUD.py
@@ -10,7 +10,7 @@ raiz.iconbitmap("C:/Users/anton/OneDrive/Escritorio/Curso de Python/Video 59 - P
 #--------------- Frame --------------- 
 miFrame = Frame(raiz, width=400, height=550)#Frame() viene siendo una hoja interna sobre la cual ahora si ponemos los elementos que conforman nuestra interface
 miFrame.config(bg="#34495E")
-miFrame.pack() #.pack() sirve para... 
+miFrame.pack() #.pack() sirve para... empaquetar el Frame en la raiz CREO xD
 
 #-------------------- Barra de menu -------------------- 
 barraMenu = Menu(raiz)
@@ -37,7 +37,28 @@ crudMenu.add_command(label="Borrar")
 ayudaMenu.add_command(label="Licencia")
 ayudaMenu.add_command(label="Acerca de...")
 
+#-------------------- Textos o Labels -------------------- 
+labelTitulo = Label(miFrame, text="CRUD Python", fg="White", bg="#34495E", font=("Rockwell", 16))
+labelTitulo.place(x=130, y=30)
+labelId = Label(miFrame, text="ID: ", fg="White", bg="#34495E", font=("Arial", 12))
+labelId.place(x=50, y=70)
+labelNombre = Label(miFrame, text="Nombre: ", fg="White", bg="#34495E", font=("Arial", 12))
+labelNombre.place(x=50, y=110)
+labelApellido = Label(miFrame, text="Apellido(s): ", fg="White", bg="#34495E", font=("Arial", 12))
+labelApellido.place(x=50, y=150)
+labelDireccion = Label(miFrame, text="Dirección: ", fg="White", bg="#34495E", font=("Arial", 12))
+labelDireccion.place(x=50, y=190)
+labelPassword = Label(miFrame, text="Contraseña: ", fg="White", bg="#34495E", font=("Arial", 12))
+labelPassword.place(x=50, y=230)
+labelComentarios = Label(miFrame, text="Comentarios: ", fg="White", bg="#34495E", font=("Arial", 12))
+labelComentarios.place(x=50, y=270)
 
+#-------------------- Cuadros de texto -------------------- 
+cuadroId = Entry(miFrame)
+cuadroId.place(x=150, y=70)
+
+
+#-------------------- Botones -------------------- 
 
 
 raiz.mainloop()#Este metodo se queda esperando a que el usuario le pase un dato
